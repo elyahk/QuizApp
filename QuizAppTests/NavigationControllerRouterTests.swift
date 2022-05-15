d786605e4f784527f1151f3022cf3f01df71219e
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-15 09:13:41 +0500

    route to second question presents second view controller

--- QuizAppTests/NavigationControllerRouterTests.swift
+++ QuizAppTests/NavigationControllerRouterTests.swift
@@ -20,7 +20,22 @@ class NavigationControllerRouterTests: XCTestCase{
 
         sut.routeTo(question: "Q1", answerCallback: { _ in })
 
-        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers, [viewController])
+    }
+
+    func test_routeToSecondQuestion_presentsQuestionViewController() {
+        let navigationController = UINavigationController()
+        let factory = NavigationControlllerFactoryStub()
+        let sut = NavigationControllerRouter(navigationController, factory: factory)
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+        factory.stub(for: "Q1", with: viewController)
+        factory.stub(for: "Q2", with: secondViewController)
+
+        sut.routeTo(question: "Q1", answerCallback: { _ in })
+        sut.routeTo(question: "Q2", answerCallback: { _ in })
+
+        XCTAssertEqual(navigationController.viewControllers, [viewController, secondViewController])
     }
 
     class NavigationControlllerFactoryStub: NavigationControllerFactory {
