a539f885cd3d7eb111563695790e0dbd29de0846
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-15 09:45:16 +0500

    Multiple question hash value return type of hash value

--- QuizAppTests/QuestionTests.swift
+++ QuizAppTests/QuestionTests.swift
@@ -16,4 +16,12 @@ class QuestionTests: XCTestCase {
 
         XCTAssertEqual(sut.hashValue, type.hashValue)
     }
+
+    func test_hashValue_multipleAnswer_returnsTypeHash() {
+        let type = "a string"
+        let sut = Question.multipleAnswer(type)
+
+        XCTAssertEqual(sut.hashValue, type.hashValue)
+    }
+
 }
