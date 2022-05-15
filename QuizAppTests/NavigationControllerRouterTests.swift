0cbe17831b7b6916c7d07381f1fd49a78b911fb6
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-15 08:50:36 +0500

    `routeTo(question:)` twice presents two `QuestionViewController`

--- QuizAppTests/NavigationControllerRouterTests.swift
+++ QuizAppTests/NavigationControllerRouterTests.swift
@@ -19,4 +19,14 @@ class NavigationControllerRouterTests: XCTestCase{
 
         XCTAssertEqual(navigationController.viewControllers.count, 1)
     }
+
+    func test_routeToQuestionTwice_presentsQuestionViewController() {
+        let navigationController = UINavigationController()
+        let sut = NavigationControllerRouter(navigationController)
+
+        sut.routeTo(question: "Q1", answerCallback: { _ in })
+        sut.routeTo(question: "Q2", answerCallback: { _ in })
+
+        XCTAssertEqual(navigationController.viewControllers.count, 2)
+    }
 }
