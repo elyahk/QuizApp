fb18c3e5daab763240c9e8d9b6d5a110efad88da
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-17 09:10:40 +0500

    Move creation of question view controller to private helper function

--- QuizApp/iOSViewNavigationControllerFactory.swift
+++ QuizApp/iOSViewNavigationControllerFactory.swift
@@ -19,10 +19,14 @@ class iOSViewNavigationControllerFactory: ViewControllerFactory {
         guard let options = self.options[question] else {
             fatalError("Couldn't find options for question: \(question)")
         }
+        return questionViewController(for: question, options: options, answerCallback: answerCallback)
+    }
 
+    private func questionViewController(for question: Question<String>, options: [String], answerCallback: @escaping ([String]) -> Void) -> UIViewController {
         switch question {
         case .singleAnswer(let value):
             return QuestionViewController(question: value, options: options, selection: answerCallback)
+
         case .multipleAnswer(let value):
             let controller = QuestionViewController(question: value, options: options, selection: answerCallback)
             _ = controller.view
