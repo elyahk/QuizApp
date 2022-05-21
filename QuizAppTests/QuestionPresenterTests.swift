3705b3302f24807aef91f7813e6f930fbca5db3d
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-05-21 16:24:20 +0500

    Refactor code and `QuestionPResenter` logic

--- QuizAppTests/QuestionPresenterTests.swift
+++ QuizAppTests/QuestionPresenterTests.swift
@@ -10,31 +10,23 @@ import XCTest
 @testable import QuizApp
 
 class QuestionPresenterTests: XCTestCase {
+    let question1 = Question.singleAnswer("Q1")
+    let question2 = Question.multipleAnswer("Q2")
+
     func test_title_forFirstQuestion_formatsTitleForIndex() {
-        let question1 = Question.singleAnswer("Q1")
-        let questions = [question1]
-        let sut = QuestionPresenter(questions: questions, question: question1)
+        let sut = QuestionPresenter(questions: [question1], question: question1)
 
         XCTAssertEqual(sut.title, "Question #1")
     }
 
     func test_title_forSecondQuestion_formatsTitleForIndex() {
-        let question1 = Question.singleAnswer("Q1")
-        let question2 = Question.multipleAnswer("Q2")
-        let questions = [question1, question2]
-
-        let sut = QuestionPresenter(questions: questions, question: question2)
+        let sut = QuestionPresenter(questions: [question1, question2], question: question2)
 
         XCTAssertEqual(sut.title, "Question #2")
     }
 
     func test_title_forUnexistenceQuestion_isEmpty() {
-        let question1 = Question.singleAnswer("Q1")
-        let question2 = Question.multipleAnswer("Q2")
-        let question3 = Question.singleAnswer("Q3")
-        let questions = [question1, question2]
-
-        let sut = QuestionPresenter(questions: questions, question: question3)
+        let sut = QuestionPresenter(questions: [], question: Question.singleAnswer("Q1"))
 
         XCTAssertEqual(sut.title, "")
     }
